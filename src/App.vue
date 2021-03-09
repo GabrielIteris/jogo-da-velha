131f19f2a70b5186c5d767c8358cdafe995d8093
Gabriel dos Santos Nascimento <gabrieldos540@gmail.com>
2021-03-09 18:57:20 -0300

    jogo da velha finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a>Projeto para ajudar povo Berberie</a>
+    <jv id="table"></jv>
+    <span> AJUDE O POVO BERBERIE </span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import jv from '@/views/jv'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  components: { jv }
 }
 </script>
 
 <style>
+a {
+  display: flexbox;
+  text-decoration: floralwhite;
+  color: white;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  justify-content: center;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Courier New, Courier, monospace;
+  float: left;
+  margin: 20px;
+  padding: 50px;
+  height: 1360px;
+  width: 880px;
+  min-width: 370px;
+  min-height: 580px;
+  background-color: burlywood;
+}
+
+#table {
+  display: flexbox;
+  justify-content: flex-end;
 }
 </style>
